bd674e43c363a79291837d7a739223b73df4194a
Dylan Hua <hua.dylan@gmail.com>
2019-05-21 17:36:07 -0500

    Checks if name and quantity are valid before adding and updates list based on SQLite database

--- app/src/main/java/com/example/supplytracker/SupplyList.kt
+++ app/src/main/java/com/example/supplytracker/SupplyList.kt
@@ -1,12 +1,10 @@
 package com.example.supplytracker
 
-import android.database.Cursor
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.DividerItemDecoration
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -15,41 +13,67 @@ import android.widget.Toast.LENGTH_SHORT
 
 class SupplyList : AppCompatActivity() {
 
-    var supplyList = ArrayList<Item>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_supply_list)
 
-        var adapter: ItemRecyclerAdapter = ItemRecyclerAdapter(supplyList, this)
+        val database = SupplyDatabase(this)
+
+        var adapter: ItemRecyclerAdapter = ItemRecyclerAdapter(this, database.getAllItems())
         var recyclerView: RecyclerView = findViewById(R.id.supply_list)
         recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(LinearLayoutManager(this));
+
 
         var itemName : EditText = findViewById(R.id.itemName)
         var itemQuantity : EditText = findViewById(R.id.itemQuantity)
 
         var addBtn : Button = findViewById(R.id.btn_add)
         addBtn.setOnClickListener(View.OnClickListener {
-            var name : String = itemName.text.toString()
-            var quantity : String = itemQuantity.text.toString()
+            try {
+                var name : String = itemName.text.toString().trim()
+                var quantity : String = itemQuantity.text.toString().trim()
 
-            if(name.length == 0) {
-                Toast.makeText(this, "Item name cannot be empty", LENGTH_SHORT).show()
-            } else if(quantity.length == 0) {
-                Toast.makeText(this, "Item quantity cannot be empty", LENGTH_SHORT).show()
-            } else {
-                var item = Item(name, quantity)
-                val db = SupplyDatabase(this)
-                if(db.addItem(item)) {
+                if(name.length == 0)
+                    throw IllegalArgumentException("Name cannot be empty")
+                var item = Item(name, quantity.toInt())
+
+                if(database.addItem(item)) {
+                    adapter.swapCursor(database.getAllItems())
+                    itemName.getText().clear()
+                    itemQuantity.getText().clear()
                     Toast.makeText(this, "Item successfully added!", LENGTH_SHORT).show()
-                    supplyList.add(item)
-                    adapter.notifyItemInserted(supplyList.indexOf(item))
                 } else {
                     Toast.makeText(this, "Item could not be added!", LENGTH_SHORT).show()
                 }
+            } catch (e : Exception) {
+                Toast.makeText(this, "Name and quantity cannot be empty.\nQuantity must have only numbers.", LENGTH_SHORT).show()
             }
-        });
+        })
+
+        var btnClear : Button = findViewById(R.id.btn_clear)
+        btnClear.setOnClickListener(View.OnClickListener {
+            database.clear()
+            adapter = ItemRecyclerAdapter(this, database.getAllItems())
+            recyclerView.setAdapter(adapter)
+            Toast.makeText(this, "List cleared!", LENGTH_SHORT).show()
+        })
+
+        var btnSortByAlphabet : Button = findViewById(R.id.btn_sort_alpha)
+        btnSortByAlphabet.setOnClickListener(View.OnClickListener {
+            adapter = ItemRecyclerAdapter(this, database.sortAlphabetically())
+            recyclerView.setAdapter(adapter)
+        })
+
+        var btnSortByNumber : Button = findViewById(R.id.btn_sort_number)
+        btnSortByNumber.setOnClickListener(View.OnClickListener {
+            adapter = ItemRecyclerAdapter(this, database.sortNumerically())
+            recyclerView.setAdapter(adapter)
+        })
     }
+
+
 }
