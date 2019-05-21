53509fd900c93970c682a722cd45f144a05c8eea
Dylan Hua <hua.dylan@gmail.com>
2019-05-21 17:33:58 -0500

    Database can clear list and sort alphabetically and numerically

--- app/src/main/java/com/example/supplytracker/SupplyDatabase.kt
+++ app/src/main/java/com/example/supplytracker/SupplyDatabase.kt
@@ -13,17 +13,18 @@ class SupplyDatabase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
         // If you change the database schema, you must increment the database version.
         private val DATABASE_VERSION = 1
         private val DATABASE_NAME = "supplydatabase.db"
-
-        private val TABLE_NAME = "Items"
-        private val COL_ID = "ID"
-        private val COL_NAME = "Name"
-        private val COL_QUANTITY = "Quantity"
     }
+
+    val TABLE_NAME = "Items"
+    val COL_ID = "ID"
+    val COL_NAME = "Name"
+    val COL_QUANTITY = "Quantity"
+    val database : SQLiteDatabase = this.writableDatabase
 
     override fun onCreate(db: SQLiteDatabase?) {
         val CREATE_TABLE = "CREATE TABLE $TABLE_NAME " +
                 "($COL_NAME VARCHAR(100), " +
-                "$COL_QUANTITY VARCHAR(100));"
+                "$COL_QUANTITY INT);"
         db!!.execSQL(CREATE_TABLE)
     }
 
@@ -35,27 +36,36 @@ class SupplyDatabase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
     }
 
     fun addItem(item : Item) : Boolean {
-        val db : SQLiteDatabase = this.writableDatabase
-
         var values : ContentValues = ContentValues().apply {
             put(COL_NAME, item.name)
             put(COL_QUANTITY, item.quantity)
         }
-        var result : Long = db?.insert(TABLE_NAME, null, values)
-        db.close()
+        var result : Long = database.insert(TABLE_NAME, null, values)
         return result > 0
     }
 
     fun deleteItem(item : Item) : Boolean {
-        val db : SQLiteDatabase = this.writableDatabase
-        return db.delete(TABLE_NAME, "$COL_NAME = \'" + item.name + "\' AND $COL_QUANTITY = \'" + item.quantity + "\'", null) > 0;
+        return database.delete(TABLE_NAME, "$COL_NAME = \'" + item.name + "\' AND $COL_QUANTITY = " + item.quantity, null) > 0;
     }
 
-    fun getItems() : Cursor {
-        val db : SQLiteDatabase = this.writableDatabase
-        val query : String = "SELECT * FROM " + TABLE_NAME
-        val data : Cursor = db.rawQuery(query, null)
-        return data
+    fun clear() {
+        database.delete(TABLE_NAME, null, null)
+    }
+
+    fun getAllItems() : Cursor {
+        return database.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+    }
+
+    fun getItem(position : Int) : Cursor {
+        return database.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+    }
+
+    fun sortAlphabetically() : Cursor {
+        return database.rawQuery("SELECT * FROM $TABLE_NAME ORDER BY " + COL_NAME + " COLLATE NOCASE ASC",null);
+    }
+
+    fun sortNumerically() : Cursor {
+        return database.rawQuery("SELECT * FROM $TABLE_NAME ORDER BY " + COL_QUANTITY,null);
     }
 }
 
