aec7f906fa961ea6e191cfc3b44f90f220f50874
Dylan Hua <hua.dylan@gmail.com>
2019-05-24 19:29:12 -0500

    Added specific check for quantity input and made code neater

--- app/src/main/java/com/example/supplytracker/SupplyList.kt
+++ app/src/main/java/com/example/supplytracker/SupplyList.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
+import java.lang.NumberFormatException
 
 class SupplyList : AppCompatActivity() {
 
@@ -22,24 +23,24 @@ class SupplyList : AppCompatActivity() {
         val database = SupplyDatabase(this)
 
         var adapter: ItemRecyclerAdapter = ItemRecyclerAdapter(this, database.getAllItems())
-        var recyclerView: RecyclerView = findViewById(R.id.supply_list)
+        val recyclerView: RecyclerView = findViewById(R.id.supply_list)
         recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(LinearLayoutManager(this));
 
 
-        var itemName : EditText = findViewById(R.id.itemName)
-        var itemQuantity : EditText = findViewById(R.id.itemQuantity)
+        val itemName : EditText = findViewById(R.id.itemName)
+        val itemQuantity : EditText = findViewById(R.id.itemQuantity)
 
-        var addBtn : Button = findViewById(R.id.btn_add)
+        val addBtn : Button = findViewById(R.id.btn_add)
         addBtn.setOnClickListener(View.OnClickListener {
             try {
-                var name : String = itemName.text.toString().trim()
-                var quantity : String = itemQuantity.text.toString().trim()
+                val name : String = itemName.text.toString().trim()
+                val quantity : String = itemQuantity.text.toString().trim()
 
                 if(name.length == 0)
                     throw IllegalArgumentException("Name cannot be empty")
-                var item = Item(name, quantity.toInt())
+                val item = Item(name, quantity.toInt())
 
                 if(database.addItem(item)) {
                     adapter.swapCursor(database.getAllItems())
@@ -47,14 +48,16 @@ class SupplyList : AppCompatActivity() {
                     itemQuantity.getText().clear()
                     Toast.makeText(this, "Item successfully added!", LENGTH_SHORT).show()
                 } else {
-                    Toast.makeText(this, "Item could not be added!", LENGTH_SHORT).show()
+                    throw Exception("Item could not be added!")
                 }
+            } catch(e : NumberFormatException) {
+                Toast.makeText(this, "Quantity must only be whole numbers", LENGTH_SHORT).show()
             } catch (e : Exception) {
-                Toast.makeText(this, "Name and quantity cannot be empty.\nQuantity must have only numbers.", LENGTH_SHORT).show()
+                Toast.makeText(this, e.message, LENGTH_SHORT).show()
             }
         })
 
-        var btnClear : Button = findViewById(R.id.btn_clear)
+        val btnClear : Button = findViewById(R.id.btn_clear)
         btnClear.setOnClickListener(View.OnClickListener {
             database.clear()
             adapter = ItemRecyclerAdapter(this, database.getAllItems())
@@ -62,13 +65,13 @@ class SupplyList : AppCompatActivity() {
             Toast.makeText(this, "List cleared!", LENGTH_SHORT).show()
         })
 
-        var btnSortByAlphabet : Button = findViewById(R.id.btn_sort_alpha)
+        val btnSortByAlphabet : Button = findViewById(R.id.btn_sort_alpha)
         btnSortByAlphabet.setOnClickListener(View.OnClickListener {
             adapter = ItemRecyclerAdapter(this, database.sortAlphabetically())
             recyclerView.setAdapter(adapter)
         })
 
-        var btnSortByNumber : Button = findViewById(R.id.btn_sort_number)
+        val btnSortByNumber : Button = findViewById(R.id.btn_sort_number)
         btnSortByNumber.setOnClickListener(View.OnClickListener {
             adapter = ItemRecyclerAdapter(this, database.sortNumerically())
             recyclerView.setAdapter(adapter)
