d23e4e5e96613ac89bb69df99b489f9ef7e13a51
Dylan Hua <hua.dylan@gmail.com>
2019-05-26 19:47:59 -0500

    Made splash screen show for 0.5 seconds and documented code

--- app/src/main/java/com/example/supplytracker/MainActivity.kt
+++ app/src/main/java/com/example/supplytracker/MainActivity.kt
@@ -4,24 +4,40 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * This class starts the application and loads the splash screen for the application.
+ * The splash screen will be displayed for a given time period. After that time period
+ * has passed, it will transition to the supply list page (SupplyList.kt).
+ */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Creates and displays splash screen.
+     * This method is called when this activity is first created.
+     *
+     * @param   savedInstanceState  Bundle containing activity's previously frozen state, if there was one
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Create a splash screen.
         val splashScreen = object : Thread() {
+            // Set up behavior for splash screen.
             override fun run() {
                 try {
-                    Thread.sleep(1000);
+                    // Try to load this splash screen for 0.5 seconds.
+                    sleep(500)
                     val intent = Intent(baseContext, SupplyList::class.java)
                     startActivity(intent)
                     finish()
                 } catch (e: Exception) {
+                    // An error occurs while displaying this splash screen.
                     e.printStackTrace()
                 }
             }
         }
+        // Display this splash screen.
         splashScreen.start()
     }
 }
