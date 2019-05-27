e9ffa3e4e45ce65c030c60e54f7f88445f172e2b
Dylan Hua <hua.dylan@gmail.com>
2019-05-26 22:57:11 -0500

    Cleaned code

--- app/src/main/java/com/example/supplytracker/Item.kt
+++ app/src/main/java/com/example/supplytracker/Item.kt
@@ -1,6 +1,7 @@
 package com.example.supplytracker
 
-class Item(name: String, quantity: Int) {
-    var name : String = name
-    var quantity : Int = quantity
-}
+/**
+ * This item has a name and an amount that can be accessed or changed.
+ * This class gets and sets the given name and the given amount for this item.
+ */
+class Item(var name: String, var amount: Int)
