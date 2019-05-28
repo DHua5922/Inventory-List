3324798f7c0a9a427c074f1157c3b797f85bda93
Dylan Hua <hua.dylan@gmail.com>
2019-05-27 20:52:39 -0500

    Displayed updated items correctly. Made display of item's name and amount change background color when clicked. Documented, modularized, and cleaned code.

--- app/src/main/java/com/example/supplytracker/ItemRecyclerAdapter.kt
+++ app/src/main/java/com/example/supplytracker/ItemRecyclerAdapter.kt
@@ -1,123 +1,265 @@
-package com.example.supplytracker;
+package com.example.supplytracker
 
 import android.app.AlertDialog
 import android.content.Context
 import android.database.Cursor
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import android.widget.Toast.LENGTH_SHORT
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TextView
 import kotlinx.android.synthetic.main.dialog_edit_field.view.*
+import android.text.InputType
 
-class ItemRecyclerAdapter(context : Context, cursor : Cursor) : RecyclerView.Adapter<ItemRecyclerAdapter.ViewHolder>() {
+/**
+ * This class manipulates the list display, using the context of the page that is displaying the list
+ * and the database iterator.
+ */
+class ItemRecyclerAdapter(private val context: Context, private var cursor: Cursor) : RecyclerView.Adapter<ItemRecyclerAdapter.ViewHolder>() {
 
-    val context = context
-    var cursor = cursor
-
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder.
-    // Each data item is just a string in this case that is shown in a TextView.
+    /**
+     * This class provides a reference to the views for each item display,
+     * using the given layout that has views for displaying item information.
+     */
     class ViewHolder(itemLayout: View) : RecyclerView.ViewHolder(itemLayout) {
-        var linearLayout : LinearLayout = itemLayout.findViewById(R.id.layoutHolder)
-        var checkBox : CheckBox = itemLayout.findViewById(R.id.checkBox)
-        var itemName : TextView = itemLayout.findViewById(R.id.name)
-        var itemQuantity : TextView = itemLayout.findViewById(R.id.quantity)
-        var deleteBtn : Button = itemLayout.findViewById(R.id.btn_delete)
+        // layout for displaying item information
+        val linearLayout : LinearLayout = itemLayout.findViewById(R.id.display_layout)
+        // checkbox for indicating if item is full or not
+        val checkBox : CheckBox = itemLayout.findViewById(R.id.checkbox)
+        // display for item name
+        val nameDisplay : TextView = itemLayout.findViewById(R.id.display_name)
+        // display for item amount
+        val amountDisplay : TextView = itemLayout.findViewById(R.id.display_amount)
+        // button for deleting item display
+        val deleteBtn : Button = itemLayout.findViewById(R.id.btn_delete)
     }
 
-
-    // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemRecyclerAdapter.ViewHolder {
-        // create a new view
+    /**
+     * Creates a new view holder that has the views for item display,
+     * using the parent view group and type of view.
+     *
+     * @param   parent      parent view group
+     * @param   viewType    type of view
+     * @return              new view holder
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // create a new view holder
         val itemLayout = LayoutInflater.from(parent.context).inflate(R.layout.template_supply_item, parent, false) as View
         return ViewHolder(itemLayout)
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
+    /**
+     * Binds events to the views in the given view holder.
+     *
+     * @param   holder      view holder
+     * @param   position    position
+     */
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        // Set item views based on your views and data model
-
         if(cursor.moveToPosition(position)) {
-            val name : String = cursor.getString(cursor.getColumnIndex("Name"))
-            val quantity : Int = cursor.getInt(cursor.getColumnIndex("Quantity"))
+            val database = SupplyDatabase(context)
 
-            holder.itemName.setText(name)
-            holder.itemName.setOnClickListener(View.OnClickListener {
-                val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_field, null)
-                val builder = AlertDialog.Builder(context).setView(dialogView)
-                val alertDialog = builder.show()
-
-                dialogView.title.text = "Replacing Item Name"
-                dialogView.description.text = "The field above will replace the item name: ${holder.itemName.text}"
-
-                dialogView.btn_dialog_ok.setOnClickListener {
-                    val newName = dialogView.field.text.toString()
-
-                    try {
-                        SupplyDatabase(context).updateName(newName, holder.itemName.text.toString())
-                        holder.itemName.text = newName
-                        alertDialog.dismiss()
-                    } catch(e : Exception) {
-                        Toast.makeText(context, e.message, LENGTH_SHORT).show()
-                    }
-                }
-
-                dialogView.btn_dialog_cancel.setOnClickListener {
-                    alertDialog.dismiss()
-                }
-            })
-
-            holder.itemQuantity.setText(quantity.toString())
-            holder.itemQuantity.setOnClickListener {
-                val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_field, null)
-                val builder = AlertDialog.Builder(context).setView(dialogView)
-                val alertDialog = builder.show()
-
-                dialogView.title.text = "Replacing Item Quantity"
-                dialogView.description.text = "The field above will replace ${holder.itemQuantity.text} as the current quantity for ${holder.itemName.text}"
-
-                dialogView.btn_dialog_ok.setOnClickListener {
-                    if(SupplyDatabase(context).updateQuantity(
-                            holder.itemName.text.toString(),
-                            dialogView.field.text.toString(),
-                            holder.itemQuantity.text.toString()
-                        )) {
-                        holder.itemQuantity.text = dialogView.field.text
-                        alertDialog.dismiss()
-                    } else {
-                        Toast.makeText(context, "Quantity must only have numbers", LENGTH_SHORT).show()
-                    }
-                }
-
-                dialogView.btn_dialog_cancel.setOnClickListener {
-                    alertDialog.dismiss()
-                }
-            }
-
-            holder.deleteBtn.setOnClickListener(View.OnClickListener {
-                val database = SupplyDatabase(context)
-
-                if(database.deleteItem(holder.itemName.text.toString())) {
-                    this.swapCursor(database.getAllItems())
-                    Toast.makeText(context, "Item successfully deleted!", LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(context, "Item could not be deleted!", LENGTH_SHORT).show()
-                }
-            })
+            // bind events to views in view holder
+            onBindCheckbox(holder, database)
+            onBindName(holder.nameDisplay, database)
+            onBindAmount(holder, database)
+            onBindDeleteBtn(holder, database)
         }
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
+    /**
+     * Gets the number of items in the list
+     *
+     * @return              number of items in the list
+     */
     override fun getItemCount() = cursor.count
 
+    /**
+     * Switches to the given database iterator to update the changes in the list.
+     *
+     * @param   newCursor   given database iterator
+     */
     fun swapCursor(newCursor : Cursor) {
         cursor.close()
         cursor = newCursor
         notifyDataSetChanged()
     }
+
+    /**
+     * When the checkbox in the given item display is clicked, color the layout of the item display
+     * as an indication if the item is full, empty, or being used. The name display is needed to
+     * check if the specific item in the given database is empty or not.
+     *
+     * @param   itemDisplay     item display
+     * @param   database        database
+     */
+    private fun onBindCheckbox(itemDisplay : ViewHolder, database : SupplyDatabase) {
+        val checkbox = itemDisplay.checkBox
+        val displayLayout = itemDisplay.linearLayout
+
+        // when checkbox is clicked
+        checkbox.setOnClickListener {
+            when {
+                // if item is full, layout of item display is colored green
+                checkbox.isChecked -> displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.isFull))
+                // if item is empty, layout of item display is colored red
+                database.getAmount("${itemDisplay.nameDisplay.text}") == 0 -> displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.isEmpty))
+                // otherwise, item still has some amount so layout of item display is colored white
+                else -> displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+            }
+        }
+    }
+
+    /**
+     * When the given name display is clicked, change the display's background color to indicate that
+     * the display is being modified and show a dialog for entering a new name for the given display
+     * of the current item name. The item in the given database is updated with the new name.
+     *
+     * @param   nameDisplay     display of item name
+     * @param   database        database
+     */
+    private fun onBindName(nameDisplay : TextView, database: SupplyDatabase) {
+        // display item name
+        nameDisplay.text = cursor.getString(cursor.getColumnIndex(SupplyDatabase.COL_NAME)).trim()
+        // when display for item name is clicked
+        nameDisplay.setOnClickListener {
+            // change display's background color to indicate it is being modified
+            nameDisplay.setBackgroundColor(ContextCompat.getColor(context, R.color.pressed_info_display))
+
+            // initialize variables for showing dialog
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_field, null)
+            val alertDialog = AlertDialog.Builder(context).setView(dialogView).show()
+            val currentName = nameDisplay.text
+
+            // if dialog is exited out, change display's background color back to default
+            alertDialog.setOnDismissListener {
+                nameDisplay.setBackgroundColor(ContextCompat.getColor(context, R.color.field))
+            }
+
+            // display dialog title
+            dialogView.title.text = context.getString(R.string.title_name_change)
+            // style and display current item name
+            dialogView.info_to_be_changed.text = TextStyle.bold("$currentName", context.getString(R.string.name_to_be_changed, currentName))
+            // describe dialog purpose
+            dialogView.description.text = context.getString(R.string.name_change_description)
+
+            // when ok button in dialog is clicked
+            dialogView.btn_dialog_ok.setOnClickListener {
+                // try to update item with new name and exit dialog
+                if(database.updateName("${dialogView.field_new_info.text}", "$currentName")) {
+                    // update list with new name
+                    swapCursor(database.getAllItems())
+                    // exit dialog
+                    alertDialog.dismiss()
+                }
+            }
+
+            // when cancel button in dialog is clicked, exit out of dialog
+            dialogView.btn_dialog_cancel.setOnClickListener {
+                alertDialog.dismiss()
+            }
+        }
+    }
+
+    /**
+     * When the amount display in the given item display is clicked, change the amount display's background color to
+     * indicate that the display is being modified and show a dialog for entering a new amount for that amount display.
+     * The name display is needed to find the specific item in the given database. That item in the database is updated
+     * with the new amount. Based on the item's new amount, color the layout as an indication if the item is empty,
+     * full, or being used.
+     *
+     * @param   itemDisplay     item display
+     * @param   database        database
+     */
+    private fun onBindAmount(itemDisplay : ViewHolder, database : SupplyDatabase) {
+        val amountDisplay = itemDisplay.amountDisplay
+
+        // display item amount
+        amountDisplay.text = "${cursor.getInt(cursor.getColumnIndex(SupplyDatabase.COL_AMOUNT))}"
+
+        val name = itemDisplay.nameDisplay.text
+        val displayLayout = itemDisplay.linearLayout
+
+        // when item is being displayed for first time, check if item is empty or not
+        if(database.getAmount("$name") == 0)
+            // item is empty so layout is colored red
+            displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.isEmpty))
+        else
+            // item is not empty so layout is colored white
+            displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+
+        // when display for item amount is clicked
+        amountDisplay.setOnClickListener {
+            // change display's background color to indicate it is being modified
+            amountDisplay.setBackgroundColor(ContextCompat.getColor(context, R.color.pressed_info_display))
+
+            // initialize variables for showing dialog
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_field, null)
+            val alertDialog = AlertDialog.Builder(context).setView(dialogView).show()
+            val currentAmount = amountDisplay.text
+
+            // if dialog is exited out, change display's background color back to default
+            alertDialog.setOnDismissListener {
+                amountDisplay.setBackgroundColor(ContextCompat.getColor(context, R.color.field))
+            }
+
+            // display dialog title
+            dialogView.title.text = context.getString(R.string.title_amount_change)
+            // style and display current item amount
+            dialogView.info_to_be_changed.text = TextStyle.bold("$currentAmount", context.getString(R.string.amount_to_be_changed, name, currentAmount))
+            // describe dialog purpose
+            dialogView.description.text = TextStyle.bold("$name", context.getString(R.string.amount_change_description, name))
+            // set number as input type for editable amount
+            dialogView.field_new_info.inputType = InputType.TYPE_CLASS_NUMBER
+
+            // when ok button in dialog is clicked
+            dialogView.btn_dialog_ok.setOnClickListener {
+                // try to update item with new amount and exit dialog
+                if(database.updateAmount(
+                        "$name",
+                        "${dialogView.field_new_info.text}".trim(),
+                        "$currentAmount")) {
+                    // update list with new amount
+                    swapCursor(database.getAllItems())
+                    // exit dialog
+                    alertDialog.dismiss()
+
+                    // when item is being updated with new amount, check if item is empty or not
+                    if(database.getAmount("$name") == 0)
+                        // item is empty so layout is colored red
+                        displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.isEmpty))
+                    else
+                        // item is not empty so layout is colored white
+                        displayLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.white))
+                }
+            }
+
+            // when cancel button in dialog is clicked, exit out of dialog
+            dialogView.btn_dialog_cancel.setOnClickListener {
+                alertDialog.dismiss()
+            }
+        }
+    }
+
+    /**
+     * When the delete button on the given item display is clicked, delete that item display.
+     * The specific item will be deleted from the database.
+     *
+     * @param   itemDisplay     item display
+     * @param   database        database
+     */
+    private fun onBindDeleteBtn(itemDisplay : ViewHolder, database : SupplyDatabase) {
+        // when delete button of item display is clicked
+        itemDisplay.deleteBtn.setOnClickListener{
+            // if item is in database, delete item and update list
+            if(database.deleteItem("${itemDisplay.nameDisplay.text}")) {
+                this.swapCursor(database.getAllItems())
+            }
+        }
+    }
+
+
 }
