20383b2ced0d5e0ca012055b756a967607ae77b0
Dylan Hua <hua.dylan@gmail.com>
2019-05-27 20:55:35 -0500

    Updated documentation and error messages. Bolded names and amount in error messages.

--- app/src/main/java/com/example/supplytracker/SupplyDatabase.kt
+++ app/src/main/java/com/example/supplytracker/SupplyDatabase.kt
@@ -5,13 +5,16 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.text.SpannableStringBuilder
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import java.lang.NumberFormatException
 
 /**
  * This supply manager accesses and manipulates the database that stores all the items.
- * This class gets the context of the page that displays the list of items.
+ * Error messages are briefly shown in the page that displays the list of items, using
+ * the context of that page.
+ *
  * Operations:
  *  - add item to database
  *  - delete item from database
@@ -29,12 +32,12 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
     // initialize variables
     private val database : SQLiteDatabase = this.writableDatabase
     companion object {
-        // If you change the database schema, you must increment the database version.
+        // if database schema is changed, increment database version
         private const val DATABASE_VERSION = 1
         private const val DATABASE_NAME = "supplydatabase.db"
         private const val TABLE_NAME = "Items"
         const val COL_NAME = "Name"
-        const val COL_QUANTITY = "Quantity"
+        const val COL_AMOUNT = "Amount"
     }
 
     /**
@@ -43,7 +46,7 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
      * @param   db  SQLite database
      */
     override fun onCreate(db: SQLiteDatabase?) {
-        db!!.execSQL("CREATE TABLE $TABLE_NAME ($COL_NAME VARCHAR(100), $COL_QUANTITY INTEGER)")
+        db!!.execSQL("CREATE TABLE $TABLE_NAME ($COL_NAME VARCHAR(100), $COL_AMOUNT INTEGER)")
     }
 
     /**
@@ -69,24 +72,29 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
     fun addItem(item : Item) : Boolean {
         val name = item.name.trim()
         val amount = item.amount
+        lateinit var styledText : SpannableStringBuilder
 
         if(name.isEmpty()) {
             Toast.makeText(context, "Name cannot be empty", LENGTH_SHORT).show()
         } else if(getItem(name).count > 0) {
-            Toast.makeText(context, "$name already exists in this list!", LENGTH_SHORT).show()
+            styledText = TextStyle.bold(name, "$name already exists in this list!")
+            Toast.makeText(context, styledText, LENGTH_SHORT).show()
         } else  {
             // prepare to add item to database
             val values: ContentValues = ContentValues().apply {
                 put(COL_NAME, name)
-                put(COL_QUANTITY, amount)
+                put(COL_AMOUNT, amount)
             }
 
             // try to add item to database
             if(database.insert(TABLE_NAME, null, values) > 0) {
-                Toast.makeText(context, "$name(quantity: $amount) added to list", LENGTH_SHORT).show()
+                styledText = TextStyle.bold(arrayOf(name, "$amount"), "$name (amount: $amount) added to list")
+                Toast.makeText(context, styledText, LENGTH_SHORT).show()
                 return true
+            // otherwise, show error message
             } else {
-                Toast.makeText(context, "$name(quantity: $amount) could not be added to list", LENGTH_SHORT).show()
+                styledText = TextStyle.bold(arrayOf(name, "$amount"), "$name (amount: $amount) could not be added to list")
+                Toast.makeText(context, styledText, LENGTH_SHORT).show()
             }
         }
 
@@ -102,13 +110,17 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
      */
     fun deleteItem(itemName : String) : Boolean {
         val name = itemName.trim()
+        lateinit var styledText : SpannableStringBuilder
 
-        // try to delete given item from database
+        // if item is in database, delete item from database
         if (database.delete(TABLE_NAME, "$COL_NAME = '$name'", null) > 0)  {
-            Toast.makeText(context, "Deleted $name from list", LENGTH_SHORT).show()
+            styledText = TextStyle.bold(name, "Deleted $name from list")
+            Toast.makeText(context, styledText, LENGTH_SHORT).show()
             return true
+        // otherwise, show error message
         } else  {
-            Toast.makeText(context, "$name is not in this list", LENGTH_SHORT).show()
+            styledText = TextStyle.bold(name, "$name is not in this list")
+            Toast.makeText(context, styledText, LENGTH_SHORT).show()
         }
 
         return false
@@ -124,23 +136,28 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
      */
     fun updateName(newName : String, oldName : String) : Boolean {
         val itemName = newName.trim()
+        lateinit var styledText : SpannableStringBuilder
 
         if(itemName.isEmpty()) {
             Toast.makeText(context, "Name cannot be empty", LENGTH_SHORT).show()
         } else if (getItem(itemName).count > 0) {
-            Toast.makeText(context, "$itemName already exists in this list", LENGTH_SHORT).show()
+            styledText = TextStyle.bold(itemName, "$itemName already exists in this list")
+            Toast.makeText(context, styledText, LENGTH_SHORT).show()
         } else {
             // prepare to update item with new name
             val newValues: ContentValues = ContentValues().apply {
                 put(COL_NAME, itemName)
             }
 
-            // try to update item with new name
+            // if item is in database, update item with new name
             if (database.update(TABLE_NAME, newValues, "$COL_NAME = '$oldName'", null) > 0) {
-                Toast.makeText(context, "$oldName replaced with $itemName", LENGTH_SHORT).show()
+                styledText = TextStyle.bold(arrayOf(oldName, itemName), "$oldName replaced with $itemName")
+                Toast.makeText(context, styledText, LENGTH_SHORT).show()
                 return true
+            // otherwise, show error message
             } else {
-                Toast.makeText(context, "Could not replace $oldName with $itemName", LENGTH_SHORT).show()
+                styledText = TextStyle.bold(arrayOf(oldName, itemName), "Could not replace $oldName with $itemName")
+                Toast.makeText(context, styledText, LENGTH_SHORT).show()
             }
         }
 
@@ -158,28 +175,34 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
      * @return              true if item has been updated with new amount, or false
      */
     fun updateAmount(itemName : String, newAmount : String, oldAmount : String) : Boolean {
-        // try to update item with new amount; otherwise, print error
+        // try to update item with new amount; otherwise, show error message
         try {
+            // initialize variables
             val name = itemName.trim()
             val newNum = newAmount.trim().toInt()
             val oldNum = oldAmount.trim().toInt()
 
             // prepare to update item with new amount
             val newValues = ContentValues().apply {
-                put(COL_QUANTITY, newNum)
+                put(COL_AMOUNT, newNum)
             }
 
-            // try to update item with new amount
+            lateinit var styledText : SpannableStringBuilder
+            // if item is in database, update item with new amount
             if(database.update(TABLE_NAME, newValues,
-                    "$COL_NAME = '$name' AND $COL_QUANTITY = $oldNum",
+                    "$COL_NAME = '$name' AND $COL_AMOUNT = $oldNum",
                     null) > 0) {
-                Toast.makeText(context, "Quantity for $name changed from $oldNum to $newNum", LENGTH_SHORT).show()
+                styledText = TextStyle.bold(arrayOf(name, "$oldNum", "$newNum"), "Amount for $name changed from $oldNum to $newNum")
+                Toast.makeText(context, styledText, LENGTH_SHORT).show()
                 return true
+            // otherwise, show error message
             } else {
-                Toast.makeText(context, "Could not change quantity for $name from $oldNum to $newNum", LENGTH_SHORT).show()
+                styledText = TextStyle.bold(arrayOf(name, "$oldNum", "$newNum"), "Could not change amount for $name from $oldNum to $newNum")
+                Toast.makeText(context, styledText, LENGTH_SHORT).show()
             }
+        // if item amount is not whole numbers, show error message
         } catch(e : NumberFormatException) {
-            Toast.makeText(context, "Quantity must only be whole numbers", LENGTH_SHORT).show()
+            Toast.makeText(context, "Amount must only be whole numbers", LENGTH_SHORT).show()
         }
 
         return false
@@ -199,8 +222,10 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
      * @return          amount of given item
      */
     fun getAmount(name : String) : Int {
-        val cursor = database.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COL_NAME = '${name.trim()}'", null)
+        // initialize initial amount as error
         var amount = -1
+        // get item in database
+        val cursor = database.rawQuery("SELECT * FROM $TABLE_NAME WHERE $COL_NAME = '${name.trim()}'", null)
 
         // move database iterator to item and get item's amount
         if (cursor.moveToFirst()) {
@@ -246,7 +271,7 @@ class SupplyDatabase(private val context: Context) : SQLiteOpenHelper(context, D
      * @return          all items sorted from lowest to highest amount in database
      */
     fun sortAmount() : Cursor {
-        return database.rawQuery("SELECT * FROM $TABLE_NAME ORDER BY $COL_QUANTITY ASC",null)
+        return database.rawQuery("SELECT * FROM $TABLE_NAME ORDER BY $COL_AMOUNT ASC",null)
     }
 }
 
