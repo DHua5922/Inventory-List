66495b16014ad9e9b9d91f85047dd7a0c5dbf948
Dylan Hua <hua.dylan@gmail.com>
2019-06-11 21:44:00 -0500

    Added migration feature from database without id column to database with id column

--- app/src/main/java/com/example/supplytracker/ItemDatabase.kt
+++ app/src/main/java/com/example/supplytracker/ItemDatabase.kt
@@ -6,6 +6,7 @@ import android.arch.persistence.room.Room
 import android.arch.persistence.room.RoomDatabase
 import android.content.Context
 import android.os.AsyncTask
+import android.arch.persistence.room.migration.Migration
 
 /**
  * This class represents a Room database that is a database
@@ -19,7 +20,7 @@ import android.os.AsyncTask
  * the version number and migrating the old database to the
  * latest version.
  */
-@Database(entities = [Item::class], version = 1)
+@Database(entities = [Item::class], version = 2, exportSchema = true)
 abstract class ItemDatabase : RoomDatabase() {
 
     abstract fun itemDao() : ItemDao
@@ -32,6 +33,31 @@ abstract class ItemDatabase : RoomDatabase() {
     companion object {
         @Volatile
         private var INSTANCE : ItemDatabase? = null
+
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Create the new table
+                database.execSQL(
+                    "CREATE TABLE table_item_new (" +
+                            "column_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " +
+                            "column_name TEXT NOT NULL, " +
+                            "column_amount REAL NOT NULL, " +
+                            "column_isFull INTEGER NOT NULL)"
+                )
+
+                // Copy data from old table to new table
+                database.execSQL(
+                    "INSERT INTO table_item_new (column_name, column_amount, column_isFull) " +
+                            "SELECT column_name, column_amount, column_isFull FROM table_item"
+                )
+
+                // Remove the old table
+                database.execSQL("DROP TABLE table_item")
+
+                // Change the table name to the correct one
+                database.execSQL("ALTER TABLE table_item_new RENAME TO table_item")
+            }
+        }
 
         /**
          * Gets a database instance. Room's database builder creates
@@ -48,8 +74,7 @@ abstract class ItemDatabase : RoomDatabase() {
                         context,
                         ItemDatabase::class.java,
                         "item_database.db"
-                    ) .fallbackToDestructiveMigration()
-                        .addCallback(roomCallback)
+                    ).addMigrations(MIGRATION_1_2)
                         .build()
                 }
             }
