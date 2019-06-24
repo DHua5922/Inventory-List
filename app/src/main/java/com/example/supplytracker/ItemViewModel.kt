6fa22c496f79f977d124c44065b26b63de63f7bd
Dylan Hua <hua.dylan@gmail.com>
2019-06-24 14:40:27 -0500

    Used Utility's print message function for printing errors or notify user of completed operations

--- app/src/main/java/com/example/supplytracker/ItemViewModel.kt
+++ app/src/main/java/com/example/supplytracker/ItemViewModel.kt
@@ -2,11 +2,7 @@ package com.example.supplytracker
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.text.SpannableStringBuilder
 import android.view.MenuItem
-import android.widget.Toast
-import android.widget.Toast.LENGTH_SHORT
-
 
 /**
  * This class represents a ViewModel that holds app's UI
@@ -19,18 +15,15 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
     private val repository: ItemRepository = ItemRepository(app)
 
     fun add(item : Item) : Long {
-        val name = item.name.trim()
-        val context = app
-        lateinit var styledText : SpannableStringBuilder
+        val name = item.name
 
         when {
-            name.isEmpty() -> Toast.makeText(context, "Name cannot be empty", LENGTH_SHORT).show()
+            name.isEmpty() -> Utility.printStyledMessage(app, "Name cannot be empty")
             else -> {
                 val result = repository.insert(item)
                 if(result > 0) {
                     val amount = item.amount
-                    styledText = TextStyle.bold(arrayOf(name, "$amount"), "$name (amount: $amount) added to list")
-                    Toast.makeText(context, styledText, LENGTH_SHORT).show()
+                    Utility.printStyledMessage(app, "$name (amount: $amount) added to list", arrayOf(name, "$amount"))
                     return result
                 }
             }
@@ -49,8 +42,7 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
                 listName = listName
             )
 
-            val id = repository.insert(newItem)
-            newItem.id = id
+            newItem.id = repository.insert(newItem)
             repository.update(newItem)
         }
     }
@@ -73,81 +65,79 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
      */
     fun sort(sortMethod : MenuItem, listName: String) : List<Item> {
         lateinit var itemList : List<Item>
-        var message = "Invalid method for sorting"
 
         // different methods to sort items
         when (sortMethod.itemId) {
             // sort by names
             R.id.option_sort_names_atoz -> {
                 itemList = repository.sortNameAToZ(listName)
-                message = "List sorted A - Z"
+                Utility.printStyledMessage(app, "List sorted A - Z")
             }
             R.id.option_sort_names_ztoa -> {
                 itemList = repository.sortNameZToA(listName)
-                message = "List sorted Z - A"
+                Utility.printStyledMessage(app, "List sorted Z - A")
             }
             R.id.option_sort_names_empty_atoz -> {
                 itemList = repository.sortNameEmptyAToZ(listName)
-                message = "Empty items sorted A - Z"
+                Utility.printStyledMessage(app, "Empty items sorted A - Z")
             }
             R.id.option_sort_names_empty_ztoa -> {
                 itemList = repository.sortNameEmptyZToA(listName)
-                message = "Empty items sorted Z - A"
+                Utility.printStyledMessage(app, "Empty items sorted Z - A")
             }
             R.id.option_sort_names_leftover_atoz -> {
                 itemList = repository.sortNameLeftoverAToZ(listName)
-                message = "Leftover items sorted A - Z"
+                Utility.printStyledMessage(app, "Leftover items sorted A - Z")
             }
             R.id.option_sort_names_leftover_ztoa -> {
                 itemList = repository.sortNameLeftoverZToA(listName)
-                message = "Leftover items sorted Z - A"
+                Utility.printStyledMessage(app, "Leftover items sorted Z - A")
             }
             R.id.option_sort_names_checked_atoz -> {
                 itemList = repository.sortNameFullAToZ(listName)
-                message = "Full (checked) items sorted A - Z"
+                Utility.printStyledMessage(app, "Full (checked) items sorted A - Z")
             }
             R.id.option_sort_names_checked_ztoa -> {
                 itemList = repository.sortNameFullZToA(listName)
-                message = "Full (checked) items sorted Z - A"
+                Utility.printStyledMessage(app, "Full (checked) items sorted Z - A")
             }
             // sort by amount
             R.id.option_sort_amount_increase -> {
                 itemList = repository.sortAmountAscending(listName)
-                message = "List sorted from lowest to highest amount"
+                Utility.printStyledMessage(app, "List sorted from lowest to highest amount")
             }
             R.id.option_sort_amount_decrease -> {
                 itemList = repository.sortAmountDescending(listName)
-                message = "List sorted from highest to lowest amount"
+                Utility.printStyledMessage(app, "List sorted from highest to lowest amount")
             }
             R.id.option_sort_amount_empty -> {
                 itemList = repository.sortAmountEmpty(listName)
-                message = "Empty items sorted"
+                Utility.printStyledMessage(app, "Empty items sorted")
             }
             R.id.option_amount_leftover_increase -> {
                 itemList = repository.sortAmountLeftoverAscending(listName)
-                message = "Leftover items sorted from lowest to highest amount"
+                Utility.printStyledMessage(app, "Leftover items sorted from lowest to highest amount")
             }
             R.id.option_sort_amount_leftover_decrease -> {
                 itemList = repository.sortAmountLeftoverDescending(listName)
-                message = "Leftover items sorted from highest to lowest amount"
+                Utility.printStyledMessage(app, "Leftover items sorted from highest to lowest amount")
             }
             R.id.option_sort_amount_full_increase -> {
                 itemList = repository.sortAmountFullAscending(listName)
-                message = "Full (checked) items sorted from lowest to highest amount"
+                Utility.printStyledMessage(app, "Full (checked) items sorted from lowest to highest amount")
             }
             R.id.option_sort_amount_full_decrease -> {
                 itemList = repository.sortAmountFullDescending(listName)
-                message = "Full (checked) items sorted from highest to lowest amount"
+                Utility.printStyledMessage(app, "Full (checked) items sorted from highest to lowest amount")
             }
             // invalid sort method
             else -> {
-                Toast.makeText(app, message, LENGTH_SHORT).show()
+                val message = "Invalid method for sorting"
+                Utility.printStyledMessage(app, message)
                 throw Exception(message)
             }
         }
 
-
-        Toast.makeText(app, message, LENGTH_SHORT).show()
         return itemList
     }
 
@@ -161,46 +151,45 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
      */
     fun delete(removalMethod : MenuItem, listName : String) : Int {
         val result: Int
-        var message = "Invalid method for removing items"
 
         // different methods to remove items
         when (removalMethod.itemId) {
             R.id.option_remove_all -> {
                 result = delete(listName)
-                message = "Removed all items"
+                Utility.printStyledMessage(app, "Removed all items")
             }
             R.id.option_remove_empty -> {
                 result = repository.deleteEmpty(listName)
-                message = "Removed all empty items"
+                Utility.printStyledMessage(app, "Removed all empty items")
             }
             R.id.option_remove_leftover -> {
                 result = repository.deleteLeftover(listName)
-                message = "Removed all leftover items"
+                Utility.printStyledMessage(app, "Removed all leftover items")
             }
             R.id.option_remove_checked -> {
                 result = repository.deleteFull(listName)
-                message = "Removed all full (checked) items"
-            } else -> {
-            Toast.makeText(app, message, LENGTH_SHORT).show()
-            throw Exception(message)
-        }
+                Utility.printStyledMessage(app, "Removed all full (checked) items")
+            }
+            else -> {
+                val message = "Invalid method for removing items"
+                Utility.printStyledMessage(app, message)
+                throw Exception(message)
+            }
         }
 
-        Toast.makeText(app, message, LENGTH_SHORT).show()
         return result
     }
 
     fun delete(item : Item) : Int {
-        lateinit var styledText : SpannableStringBuilder
-        val name = item.name.trim()
+        val name = item.name
         val result = repository.delete(item)
-        if(result > 0) {
-            styledText = TextStyle.bold(name, "$name has been deleted")
-            Toast.makeText(app, styledText, LENGTH_SHORT).show()
+
+        val message = if(result > 0) {
+            "$name has been deleted"
         } else {
-            styledText = TextStyle.bold(name, "$name could not be deleted")
-            Toast.makeText(app, styledText, LENGTH_SHORT).show()
+            "$name could not be deleted"
         }
+        Utility.printStyledMessage(app, message, arrayOf(name))
         return result
     }
 
@@ -225,7 +214,7 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
         // different methods to search items
         when (searchMethod.itemId) {
             R.id.option_search_name -> {
-                itemList = repository.getItemByName(word.trim(), listName)
+                itemList = repository.getItemByName(word, listName)
             }
             R.id.option_search_amount -> {
                 when (comparison) {
@@ -254,7 +243,7 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
             }
             else -> {
                 val message = "Invalid method for searching items"
-                Toast.makeText(app, message, LENGTH_SHORT).show()
+                Utility.printStyledMessage(app, message)
                 throw Exception(message)
             }
         }
