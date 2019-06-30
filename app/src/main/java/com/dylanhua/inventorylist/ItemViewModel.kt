8a08aa6fa0a870c607cabf8bb4667ef7cb199214
Dylan Hua <hua.dylan@gmail.com>
2019-06-29 20:36:39 -0500

    Removed unnecessary messages for sorting items. Passed context parameters so that toast messages can be responsive to different screen sizes. Cleaned code.

--- app/src/main/java/com/dylanhua/inventorylist/ItemViewModel.kt
+++ app/src/main/java/com/dylanhua/inventorylist/ItemViewModel.kt
@@ -1,6 +1,7 @@
 package com.dylanhua.inventorylist
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import android.view.MenuItem
 
@@ -11,21 +12,20 @@ import android.view.MenuItem
  * are held and processed in this ViewModel.
  */
 class ItemViewModel(application : Application) : AndroidViewModel(application) {
-    private val app: Application = application
-    private val repository: ItemRepository = ItemRepository(app)
+    private val repository: ItemRepository = ItemRepository(application)
 
     /**
      * Adds the given item to the current list.
      *
      * @param   item      given item
+     * @param   context   given activity context
      * @return            rowId of inserted item
      */
-    fun add(item : Item) : Long {
+    fun add(item : Item, context : Context) : Long {
         val name = item.name
-
         when {
             name.isEmpty() -> Utility.printStyledMessage(
-                app,
+                context,
                 "Name cannot be empty"
             )
             else -> {
@@ -33,7 +33,7 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
                 if(result > 0) {
                     val amount = item.amount
                     Utility.printStyledMessage(
-                        app,
+                        context,
                         "$name (amount: $amount) added to list",
                         arrayOf(name, "$amount")
                     )
@@ -88,7 +88,6 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
 
     /**
      * Sorts all or certain items in the given list by their names or amount based on chosen sort method.
-     * Throws an exception if the chosen method for sorting is invalid.
      *
      * @param       sortMethod  chosen sort method
      * @param       listName    given list
@@ -103,94 +102,49 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
             // sort by names
             R.id.option_sort_names_atoz -> {
                 itemList = repository.sortNameAToZ(listName)
-                Utility.printStyledMessage(app, "List sorted A - Z")
             }
             R.id.option_sort_names_ztoa -> {
                 itemList = repository.sortNameZToA(listName)
-                Utility.printStyledMessage(app, "List sorted Z - A")
             }
             R.id.option_sort_names_empty_atoz -> {
                 itemList = repository.sortNameEmptyAToZ(listName)
-                Utility.printStyledMessage(app, "Empty items sorted A - Z")
             }
             R.id.option_sort_names_empty_ztoa -> {
                 itemList = repository.sortNameEmptyZToA(listName)
-                Utility.printStyledMessage(app, "Empty items sorted Z - A")
             }
             R.id.option_sort_names_leftover_atoz -> {
                 itemList = repository.sortNameLeftoverAToZ(listName)
-                Utility.printStyledMessage(app, "Leftover items sorted A - Z")
             }
             R.id.option_sort_names_leftover_ztoa -> {
                 itemList = repository.sortNameLeftoverZToA(listName)
-                Utility.printStyledMessage(app, "Leftover items sorted Z - A")
             }
             R.id.option_sort_names_checked_atoz -> {
                 itemList = repository.sortNameFullAToZ(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "Full (checked) items sorted A - Z"
-                )
             }
             R.id.option_sort_names_checked_ztoa -> {
                 itemList = repository.sortNameFullZToA(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "Full (checked) items sorted Z - A"
-                )
             }
             // sort by amount
             R.id.option_sort_amount_increase -> {
                 itemList = repository.sortAmountAscending(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "List sorted from lowest to highest amount"
-                )
             }
             R.id.option_sort_amount_decrease -> {
                 itemList = repository.sortAmountDescending(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "List sorted from highest to lowest amount"
-                )
             }
             R.id.option_sort_amount_empty -> {
                 itemList = repository.sortAmountEmpty(listName)
-                Utility.printStyledMessage(app, "Empty items sorted")
             }
             R.id.option_amount_leftover_increase -> {
                 itemList = repository.sortAmountLeftoverAscending(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "Leftover items sorted from lowest to highest amount"
-                )
             }
             R.id.option_sort_amount_leftover_decrease -> {
                 itemList = repository.sortAmountLeftoverDescending(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "Leftover items sorted from highest to lowest amount"
-                )
             }
             R.id.option_sort_amount_full_increase -> {
                 itemList = repository.sortAmountFullAscending(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "Full (checked) items sorted from lowest to highest amount"
-                )
             }
             R.id.option_sort_amount_full_decrease -> {
                 itemList = repository.sortAmountFullDescending(listName)
-                Utility.printStyledMessage(
-                    app,
-                    "Full (checked) items sorted from highest to lowest amount"
-                )
-            }
-            // invalid sort method
-            else -> {
-                val message = "Invalid method for sorting"
-                Utility.printStyledMessage(app, message)
-                throw Exception(message)
             }
         }
 
@@ -199,42 +153,36 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
 
     /**
      * Deletes all or certain items from the given list based on the chosen removal method.
-     * Throws an exception if the chosen method for removing is invalid.
      *
      * @param       removalMethod   chosen removal method
      * @param       listName        given list
-     * @exception   Exception       if remove method is invalid
+     * @param       context         given activity context
      * @return                      number of rows deleted
      */
-    fun delete(removalMethod : MenuItem, listName : String) : Int {
-        val result: Int
+    fun delete(removalMethod : MenuItem, listName : String, context : Context) : Int {
+        var result: Int = -1
 
         // remove items based on chosen method
         when (removalMethod.itemId) {
             R.id.option_delete_list_all -> {
                 result = repository.deleteAllLists()
-                Utility.printStyledMessage(app, "Removed all lists")
+                Utility.printStyledMessage(context, "Removed all inventories")
             }
             R.id.option_remove_all -> {
                 result = delete(listName)
-                Utility.printStyledMessage(app, "Removed all items")
+                Utility.printStyledMessage(context, "Removed all items")
             }
             R.id.option_remove_empty -> {
                 result = repository.deleteEmpty(listName)
-                Utility.printStyledMessage(app, "Removed all empty items")
+                Utility.printStyledMessage(context, "Removed all empty items")
             }
             R.id.option_remove_leftover -> {
                 result = repository.deleteLeftover(listName)
-                Utility.printStyledMessage(app, "Removed all leftover items")
+                Utility.printStyledMessage(context, "Removed all leftover items")
             }
             R.id.option_remove_checked -> {
                 result = repository.deleteFull(listName)
-                Utility.printStyledMessage(app, "Removed all full (checked) items")
-            }
-            else -> {
-                val message = "Invalid method for removing items"
-                Utility.printStyledMessage(app, message)
-                throw Exception(message)
+                Utility.printStyledMessage(context, "Removed all full (checked) items")
             }
         }
 
@@ -244,20 +192,11 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
     /**
      * Deletes the given item from the database.
      *
-     * @param   item    given item
-     * @return          number of rows deleted
+     * @param   item        given item
+     * @return              number of rows deleted
      */
     fun delete(item : Item) : Int {
-        val name = item.name
-        val result = repository.delete(item)
-
-        val message = if(result > 0) {
-            "$name has been deleted"
-        } else {
-            "$name could not be deleted"
-        }
-        Utility.printStyledMessage(app, message, arrayOf(name))
-        return result
+        return repository.delete(item)
     }
 
     /**
@@ -272,14 +211,12 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
 
     /**
      * Searches all or certain items in the given list based on the chosen search method.
-     * Throws an exception if the chosen method for searching is invalid.
      *
      * @param       searchMethod    chosen search method
      * @param       listName        given list
      * @param       word            name or keyword to search item for
      * @param       amount          exact amount to search items for
      * @param       comparison      comparison for searching items with more than exact amount
-     * @exception   Exception       if search method is invalid
      * @return                      chosen items to search for
      */
     fun search(searchMethod : MenuItem, listName: String, word : String = "", amount : Double = -1.0, comparison : Int = -1) : List<Item> {
@@ -314,11 +251,6 @@ class ItemViewModel(application : Application) : AndroidViewModel(application) {
             }
             R.id.option_search_all -> {
                 itemList = repository.getAllItems(listName)
-            }
-            else -> {
-                val message = "Invalid method for searching items"
-                Utility.printStyledMessage(app, message)
-                throw Exception(message)
             }
         }
 
